fbca11364dbddca9d13f4355319b6ff75a465a3d
Ashwani Kumar Dwivedi <ashwanidwivedi7898@gmail.com>
2025-07-15 13:36:32 +0530

    fixed with war packaging and using postgresql

--- src/main/java/com/ashwanicoding/hotel_management/HotelManagementApplication.java
+++ src/main/java/com/ashwanicoding/hotel_management/HotelManagementApplication.java
@@ -2,9 +2,10 @@ package com.ashwanicoding.hotel_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HotelManagementApplication {
+public class HotelManagementApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelManagementApplication.class, args);
